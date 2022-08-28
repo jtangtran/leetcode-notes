a7e7d424d48b70b0c3cff38e0e6ceb148d80b7b5
Jennifer <phuongtangtran@gmail.com>
2022-08-28 09:25:28 -0700

    updated and added second submission

--- leetcode.js
+++ leetcode.js
@@ -31,5 +31,49 @@ var containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
 };
 
+//Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//First Submission:
+var isAnagram = function(s, t) {
+    let strArr = t.split('').sort();
+    let sArr = s.split('').sort();
+    if (strArr.length != sArr.length) {
+        return false;
+    }
+    let idx = 0;
+    for (let character of strArr) {
+        if (character === sArr[idx]) {
+            idx++;
+            continue;
+        }
+        return false;
+    }
+    return true;
+};
 
+//implemented with using map
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+    let sMap = {};
+    let tMap = {};
+    for (let i = 0; i < s.length; i++) {
+        if (sMap.hasOwnProperty(s[i])) {
+            sMap[s[i]]++;
+        } else {
+            sMap[s[i]] = 1;
+        }
+        if (tMap.hasOwnProperty(t[i])) {
+            tMap[t[i]]++;
+        } else {
+            tMap[t[i]] = 1;
+        }
+    }
+    for (let k in sMap) {
+        if (sMap[k] !== tMap[k]) {
+            return false;
+        }
+    }
+    return true;
+};
 
